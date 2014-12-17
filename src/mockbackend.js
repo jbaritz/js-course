4e14e5a008dec2a397195578ab0cf96224b180d7
Fernando Catacora <nando.drw@gmail.com>
2014-12-17 02:00:35 -0500

    finish with mock backend

--- src/mockbackend.js
+++ src/mockbackend.js
@@ -1,8 +1,179 @@
 (function(){
 
-  $.mockjax({
-    url: "/FBI/case",
-    proxy: "/mocks/data.json"
-  });
+  var validateInterview = function (data){
+    if(!data || typeof(data) != "string"){
+      return false
+    }
+    if(data.localeCompare('Truck drivers declarations.') == 0) {
+      return "/mocks/interview1.json";
+    }
+    if(data.localeCompare('A kid was playing close to one of the crime scenes.') == 0) {
+      return "/mocks/interview2.json";
+    }
+    return false;
+  };
+
+  var validateIntem = function (data){
+    if(!data || typeof(data) != "string"){
+      return false
+    }
+    if(data.localeCompare("flat tires on all the trucks.") == 0) {
+      return "/mocks/item1.json";
+    }
+    if(data.localeCompare("Shoe prints on the ground.") == 0) {
+      return "/mocks/item2.json";
+    }
+    return false;
+  };
+
+  var validateInjure = function (data){
+    if(!data || typeof(data) != "string"){
+      return false
+    }
+    if(data.localeCompare("Some truck drivers got a hit on the head.") == 0) {
+      return "/mocks/medical.json";
+    }
+    return false;
+  };
+
+  var validateSuspectsList = function (data){
+    if(!data || typeof(data) != "string"){
+      return false
+    }
+    if(data.localeCompare("Vito Corleone") == 0) {
+      return "/mocks/vc.json";
+    }
+    if(data.localeCompare("Just a weird guy") == 0) {
+      return "/mocks/wg.json";
+    }
+    if(data.localeCompare("Old fashion Italan Mafia Style.") == 0) {
+      return "/mocks/ms.json";
+    }
+    if(data.localeCompare("Cowboy's shoe prints.") == 0) {
+      return "/mocks/cw.json";
+    }
+    if(data.localeCompare("Fake Gangsta") == 0) {
+      return "/mocks/fg.json";
+    }
+    return false;
+  };
+
+  var validateRankInput = function (rankInfo) {
+    if(!rankInfo ||
+      !rankInfo.accuracy ||
+      !rankInfo.weight ||
+      !(rankInfo.accuracy < 1 && rankInfo.accuracy > 0) ||
+      !(typeof(rankInfo.weight) === 'number')) {
+      return false;
+    }
+    return rankInfo.weight * rankInfo.accuracy;
+  }
+
+  $.mockjax(function(settings){
+    console.log("settings!!",settings)
+    switch(settings.url){
+
+      case '/FBI/API/case' :
+        if(settings.type === 'GET') {
+          return {
+            responseTime : 1000,
+            proxy: "/mocks/case.json"
+          };
+        }
+        return {
+          responseTime : 500,
+          responseText : "Invalid request",
+          status : 401
+        };
+
+      case "/FBI/API/interview" :
+        if(settings.data && settings.type === 'POST'){
+          if(settings.data.caseId == 'case-01'
+          && validateInterview(settings.data.who)){
+            return {
+              responseTime : 1500,
+              proxy : validateInterview(settings.data.who)
+            };
+          }
+        }
+        return {
+          responseTime : 500,
+          responseText : "Invalid request",
+          status : 401
+        };
+
+      case "/FBI/API/itemLaboratory" :
+        if(settings.data && settings.type === 'POST'){
+          if(settings.data.caseId == 'case-01'
+          && validateIntem(settings.data.what)){
+            return {
+              responseTime : 1500,
+              proxy : validateIntem(settings.data.what)
+            };
+          }
+        }
+        return {
+          responseTime : 500,
+          responseText : "Invalid request",
+          status : 401
+        };
+
+      case "/FBI/API/medicalAnalisys" :
+        if(settings.data && settings.type === 'POST'){
+          if(settings.data.caseId == 'case-01'
+          && validateInjure(settings.data.what)){
+            return {
+              responseTime : 2000,
+              proxy : validateInjure(settings.data.what)
+            };
+          }
+        }
+        return {
+          responseTime : 500,
+          responseText : "Invalid request",
+          status : 401
+        };
+
+      case "/FBI/API/suspectsList" :
+        if(settings.data && settings.type === 'GET'){
+          if(settings.data.caseId == 'case-01'
+          && validateSuspectsList(settings.data.characteristics)){
+            return {
+              responseTime : 1100,
+              proxy : validateSuspectsList(settings.data.characteristics)
+            };
+          }
+        }
+        return {
+          responseTime : 500,
+          responseText : "Invalid request :(",
+          status : 401
+        };
+
+      case "/FBI/API/calculateScore" :
+        if(settings.data && settings.type === 'POST'){
+          if(settings.data.caseId == 'case-01'
+          && validateRankInput(settings.data.rankInput)){
+            return {
+              responseTime : 1100,
+              responseText : JSON.stringify(
+                {result : validateRankInput(settings.data.rankInput)}
+              )
+            };
+          }
+        }
+        return {
+          responseTime : 500,
+          responseText : "Invalid request",
+          status : 401
+        };
+
+    };
+    return {
+      responseTime : 500,
+      responseText : "Invalid request",
+      status : 401
+    };
+  })
 
 })();
